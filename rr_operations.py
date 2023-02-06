2582621e75711c84198db5a464ab5404b2af94c7
Bradley Hazelton <bradley@thehazeltons.ca>
2023-02-05 21:38:07 -0700

    switched to using pyarr

--- rr_operations.py
+++ rr_operations.py
@@ -1,4 +1,4 @@
-import requests
+import pyarr
 
 def remove_movie_from_radarr(movie_id, radarr_api_url, api_key):
     """
@@ -12,26 +12,11 @@ def remove_movie_from_radarr(movie_id, radarr_api_url, api_key):
     Returns:
     None
     """
-    # Define endpoint, header, and timeout
-    endpoint = f"{radarr_api_url}{movie_id}"
-    header = {'X-api-key': api_key}
-    timeout = 5  # in seconds
+    # Connect to Radarr API
+    radarr = pyarr.Radarr(radarr_api_url, api_key)
 
-    # Delete movie from Radarr and add to exclusion list
-    params = {'addImportExclusion': True, 'deleteFiles': False}
+    # Remove movie from Radarr and add to exclusion list
     try:
-        response = requests.delete(endpoint, headers=header, params=params, timeout=timeout)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as error:
-        if response.status_code == 400:
-            print("Bad Request: The request was invalid.")
-        elif response.status_code == 401:
-            print("Unauthorized: The API key is invalid.")
-        elif response.status_code == 404:
-            print("Not Found: The movie was not found in Radarr.")
-        else:
-            print(f"HTTP Error: {error}")
-    except requests.exceptions.Timeout as error:
-        print(f"Request Timeout: The API endpoint did not respond within {timeout} seconds.")
-    except requests.exceptions.RequestException as error:
-        print(f"Request Error: {error}")
+        radarr.movie.remove(movie_id, add_import_exclusion=True, delete_files=False)
+    except pyarr.exceptions.RadarrError as error:
+        print(f"Error: {error}")
